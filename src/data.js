3102459fd0597f71ecd55e691d81d29291958b67
Mensur Durakovic <mensurdurakovic5@gmail.com>
2021-01-27 18:54:07 +0100

    added new console log

--- src/data.js
+++ src/data.js
@@ -7,6 +7,8 @@ function fetchCountries(term) {
   const countriesUrl = `https://restcountries.eu/rest/v2/name/${term}`;
   console.log("countriesUrl", countriesUrl);
 
+  console.log("nova linija");
+
   return new Promise((resolve) => {
     axios
       .get(countriesUrl)
